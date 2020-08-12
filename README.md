eb517008ccc1800d1985b8ffa878b7db0232f9de
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-08-11 22:18:30 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # imghostr
-ImgHostr is my solution to a simple ShareX server in NodeJS.
+ImgHostr is a one-dependency ShareX server written in NodeJS.
+
+## Features
+
+- Dead simple setup
+- Customizable homepage
+
+## Installation
+
+### Requirements
+- NodeJS 10 or above.
+- Port ``3000`` to be open (changeable via ``index.js``).
+
+### Steps
+
+1. Make sure node is up to date!
+2. Install dependency (via ``npm install``).
+3. (optional) Edit ``conf`` to your liking.
+4. Run ``node index``. 
+5. Enjoy!
---
+++ index.js
@@ -0,0 +1,107 @@
+const http = require("http");
+const fs = require("fs");
+const parseFormdata = require('parse-formdata');
+const url = require("url");
+const conf = {
+    port: 3000,
+    url: "http://localhost:3000",
+    maxSize: 20000000
+}
+startUp();
+
+function startUp() {
+    if (!fs.existsSync("./images/")) {
+        fs.mkdirSync("./images/");
+        console.log("[i] created images folder!");
+    }
+    http.createServer(hostServer).listen(conf.port);
+    console.log("[i] started server at port " + conf.port);
+}
+
+function createId() {
+    // based off of https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+    var result = "";
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+    for (var c = 0; c < 10; c++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+function hostServer(request, response) {
+    var u = url.parse(request.url, true);
+    if (u.pathname == "/upload" && request.method == "POST") {
+        for (var c in request.rawHeaders) {
+            if (request.rawHeaders) {
+                if (request.rawHeaders[c] == "Content-Length") {
+                    var d = parseInt(c) + 1
+                    var size = parseInt(request.rawHeaders[d]);
+                    if (size > conf.maxSize) {
+                        request.end("too big! compress your image!");
+                        return;
+                    }
+                }
+            }
+        }
+        parseFormdata(request, function (err, data) {
+            if (!err && data.parts[0]) {
+                var id = createId();
+                fs.appendFileSync("./images/" + id + ".png");
+                var d = fs.createWriteStream("./images/" + id + ".png");
+                data.parts[0].stream.pipe(d);
+                var endData = JSON.stringify({
+                    "file": conf.url + "/" + id
+                })
+                response.end(endData);
+            } else if (!err) {
+                var endData = "invalid body";
+                response.end(endData);
+            } else {
+                var endData = err.code;
+                response.end(endData);
+            }
+        })
+    } else {
+        if (u.pathname == "/" | u.pathname == "/index.html") {
+            fs.readFile("./html/index.html", function(err, resp) {
+                if (!err) {
+                    response.writeHead(200, {
+                        "Access-Control-Allow-Origin": "*",
+                        "Content-Type": "text/html"
+                    })
+                    response.end(resp);
+                } else {
+                    response.writeHead(404, {
+                        "Access-Control-Allow-Origin": "*",
+                        "Content-Type": "text/plain"
+                    })
+                    response.end("welcome to your imghostr server!");
+                }
+            })
+        } else {
+            fs.readFile("./images" + u.pathname + ".png", function(err, resp) {
+                if (!err) {
+                    response.writeHead(200, {
+                        "Access-Control-Allow-Origin":"*",
+                        "Content-Type": "image/png"
+                    })
+                    response.end(resp)
+                } else {
+                    if (err.code == "ENOENT") {
+                        response.writeHead(404, {
+                            "Access-Control-Allow-Origin":"*",
+                            "Content-Type": "text/plain"
+                        })
+                        response.end("404 - image does not exist");
+                    } else {
+                        response.writeHead(501, {
+                            "Access-Control-Allow-Origin":"*",
+                            "Content-Type": "image/png"
+                        })
+                        response.end(err.code);
+                    }
+                }
+            })
+        }
+    }
+}
